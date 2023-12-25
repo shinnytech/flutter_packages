57ffd7a20cd2628cfd6697c71e4837b420b2ac82
法的空间 <zmtzawqlp@live.com>
2023-12-25 08:45:42 +0000

    增加缺失的引用 增加缺失的引用
    Signed-off-by: 法的空间 <zmtzawqlp@live.com>

--- packages/pigeon/lib/arkts_generator.dart
+++ packages/pigeon/lib/arkts_generator.dart
@@ -91,9 +91,7 @@ class ArkTSGenerator extends StructuredGenerator<ArkTSOptions> {
     indent.writeln(
         "import StandardMessageCodec from '@ohos/flutter_ohos/src/main/ets/plugin/common/StandardMessageCodec';");
     indent.writeln(
-        "import BasicMessageChannel from '@ohos/flutter_ohos/src/main/ets/plugin/common/BasicMessageChannel';");
-    indent.writeln(
-        "import { BinaryMessenger,TaskQueue } from '@ohos/flutter_ohos/src/main/ets/plugin/common/BinaryMessenger';");
+        "import { BasicMessageChannel,BinaryMessenger,TaskQueue,Reply} from '@ohos/flutter_ohos/src/main/ets/plugin/common/BinaryMessenger';");
     indent.writeln(
         "import MessageCodec from '@ohos/flutter_ohos/src/main/ets/plugin/common/MessageCodec';");
     indent.writeln(
