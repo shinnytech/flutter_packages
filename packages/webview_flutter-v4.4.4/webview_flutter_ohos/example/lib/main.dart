bd3e7b91d5a225493338f2afdba07f24a34275e8
liuyuehu <3455278857@qq.com>
2024-03-16 12:14:27 +0800

    webview-v4.4.4 Signed-off-by: liuyuehu <3455278857@qq.com>

---
+++ packages/webview_flutter-v4.4.4/webview_flutter_ohos/example/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Welcome to Flutter',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Welcome to Flutter'),
+        ),
+        body: new Center(
+          child: new Text('你好开发者，webview样例还未更新'),
+        ),
+      ),
+    );
+  }
+}
