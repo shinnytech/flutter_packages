1d257d4315dd7540be215fb90359bf9b53427e9e
hyiso <mylaoda@qq.com>
2024-04-15 15:03:27 +0800

    webview_flutter_ohos 适配重构后的结构
    Signed-off-by: hyiso <mylaoda@qq.com>

--- packages/webview_flutter/webview_flutter_ohos/ohos/hvigorfile.ts
+++ packages/webview_flutter/webview_flutter_ohos/ohos/hvigorfile.ts
@@ -13,9 +13,9 @@
 * limitations under the License.
 */
 
-import { appTasks } from '@ohos/hvigor-ohos-plugin';
+import { harTasks } from '@ohos/hvigor-ohos-plugin';
 
 export default {
-    system: appTasks,  /* Built-in plugin of Hvigor. It cannot be modified. */
+    system: harTasks,  /* Built-in plugin of Hvigor. It cannot be modified. */
     plugins:[]         /* Custom plugin to extend the functionality of Hvigor. */
 }
