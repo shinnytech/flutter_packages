d0afe5af01a86f0986642f2f1d9f45c1e6f19245
huangxiaoyao <976125628@qq.com>
2024-09-25 14:14:48 +0800

    fix: file_selector_ohos的sample执行build报错
    Signed-off-by: huangxiaoyao <976125628@qq.com>

--- packages/file_selector/file_selector_ohos/example/lib/open_text_page.dart
+++ packages/file_selector/file_selector_ohos/example/lib/open_text_page.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:convert';
 import 'package:file_selector_platform_interface/file_selector_platform_interface.dart';
 import 'package:flutter/material.dart';
 
