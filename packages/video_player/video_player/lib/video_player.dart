e2d3a31ddf3c2e95129f2fec76854ebc2e63a30b
wwyang <137208408@qq.com>
2024-12-17 15:05:16 +0800

    解决播放完成重复播放失败的问题
    Signed-off-by: wwyang <137208408@qq.com>

--- packages/video_player/video_player/lib/video_player.dart
+++ packages/video_player/video_player/lib/video_player.dart
@@ -468,7 +468,7 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
           // position=value.duration. Instead of setting the values directly,
           // we use pause() and seekTo() to ensure the platform stops playing
           // and seeks to the last frame of the video.
-          pause().then((void pauseResult) => seekTo(value.duration));
+          pause().then((void pauseResult) => seekTo(Duration.zero));
           break;
         case VideoEventType.bufferingUpdate:
           value = value.copyWith(buffered: event.buffered);
