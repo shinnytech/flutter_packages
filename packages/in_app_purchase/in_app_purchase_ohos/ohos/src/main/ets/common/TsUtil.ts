77c19a63512a359b92728b21f787a7dfe67c042a
huangxiaoyao <976125628@qq.com>
2025-01-20 19:20:15 +0800

    修改文件版权头
    Signed-off-by: huangxiaoyao <976125628@qq.com>

--- packages/in_app_purchase/in_app_purchase_ohos/ohos/src/main/ets/common/TsUtil.ts
+++ packages/in_app_purchase/in_app_purchase_ohos/ohos/src/main/ets/common/TsUtil.ts
@@ -1,3 +1,7 @@
+// Copyright (c) 2024 SwanLink (Jiangsu) Technology Development Co., LTD.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE_ODID file.
+
 export function ObjToMap(obj: Object): Map<string, Object> {
   const map = new Map();
   for (const key in obj) {
